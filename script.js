39760198514faabe300e9fb748a9d98e8fb4f011
DeveloperBuilder <35078547+DeveloperBuilder@users.noreply.github.com>
2021-10-03 11:40:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hallo Winc Academy studenten")
+
+/*1. Bereken de uitkomst van 1230941 x 1823794 en 
+log de uitkomst naar de console (in dit geval jouw terminal).*/
+let vermenigvuldigen = 1230941 * 1823794;
+console.log("Vermenigvuldigen", vermenigvuldigen)
+
+/*Bereken de uitkomst van 637263 / 54 en 
+log de uitkomst naar de console (in dit geval jouw terminal).*/
+let delen = 637263 / 54;
+console.log("Delen", delen)
+
+//3. Dit is een grote som". 4. Een stukje code dat in een comment staat, doet niet meer mee voor de JavaScript engine. 
+
+//5. je ziet de onderstaande code niet als je blok comment doet.
+/*let vermenigvuldigen = 1230941 * 1823794;
+console.log("Vermenigvuldigen", vermenigvuldigen)
+
+let delen = 637263 / 54;
+console.log("Delen", delen)*/
+
